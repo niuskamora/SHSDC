0952fb0038553f9e85fd34a316c36b7e2136a089
Mariela Arboleda <luz.arboleda25@gmail.com>
2014-04-24 17:28:02 -0430

    acento

--- views/create_user.php
+++ views/create_user.php
@@ -6,7 +6,7 @@ if (!isset($Sedes)) {
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="ISO-8859-1">
+        <meta charset="utf-8">
         <title>Seguros Horizonte | HorizonLine</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="">
